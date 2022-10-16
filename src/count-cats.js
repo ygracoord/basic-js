21838220ab1b732b110947ef98933dce8d440ba9
ygracoord <ygra.pl@gmail.com>
2022-10-16 14:44:29 +0300

    feat: complete count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+    let numberOfEars = backyard.flat(backyard.length).filter(el => el == '^^');
+    return numberOfEars.length;
 }
 
 module.exports = {
   countCats
 };
+
